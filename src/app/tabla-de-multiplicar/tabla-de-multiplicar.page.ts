99b80f8bd947e2f10531f499e4bb3191b1f103ea
Ravelo-Dev <zmarcos140@gmail.com>
2024-09-26 09:11:18 -0400

    Generador de tablas listo

--- src/app/tabla-de-multiplicar/tabla-de-multiplicar.page.ts
+++ src/app/tabla-de-multiplicar/tabla-de-multiplicar.page.ts
@@ -8,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class TablaDeMultiplicarPage implements OnInit {
 
   constructor() { }
+  num: number = 1;
+
+  tablaVisible: boolean = false;
+
+  rango: number[] = [1,2,3,4,5,6,7,8,9,10,11,12];
+  generarTabla() {
+    this.tablaVisible = true;
+  }
 
   ngOnInit() {
   }
